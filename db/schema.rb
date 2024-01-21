f8619e55b1a308647b7e6cb1fe3868776e24333f
MinPyaeHein <minpyaehein.ucsdawei@gmail.com>
2024-01-22 01:39:57 +0700

    user email lower case

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_21_144144) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_154159) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_21_144144) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
