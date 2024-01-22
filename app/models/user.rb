e3c424ebd6cb009a6b8701c5f7e00061cf4815c7
MinPyaeHein <minpyaehein.ucsdawei@gmail.com>
2024-01-22 21:31:46 +0700

    sing up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+   
     before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, 
-                        uniqueness: { case_sensitive: false }, 
-
+                        uniqueness: { case_sensitive: false } 
+has_secure_password
 end
